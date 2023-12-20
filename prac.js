ed0700ad5d416c54b4e0d7a8863ae8fbad6d96c6
Baromental <suzdalcevmaksym@gmail.com>
2023-12-20 15:59:10 +0200

    task done

--- prac.js
+++ prac.js
@@ -1,16 +1,16 @@
-function getOrderQuantity(order) {
-  console.log(order.length)
-}
-getOrderQuantity(["apple", "peach", "pear", "banana"])
-getOrderQuantity(["apple", "banana"]);
-getOrderQuantity(["apple", "banana", "pear"]);
-getOrderQuantity([]);
-
-
 // function getOrderQuantity(order) {
-//   console.log(order.length)
+//   return console.log(order.length)
 // }
 // getOrderQuantity(["apple", "peach", "pear", "banana"])
 // getOrderQuantity(["apple", "banana"]);
 // getOrderQuantity(["apple", "banana", "pear"]);
 // getOrderQuantity([]);
+
+
+function getOrderQuantity(order) {
+  return order.length
+}
+console.log(getOrderQuantity(["apple", "peach", "pear", "banana"]));
+console.log(getOrderQuantity(["apple", "banana"]));
+console.log(getOrderQuantity(["apple", "banana", "pear"]));
+console.log(getOrderQuantity([]));
